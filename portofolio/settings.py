fe09c79d47a4729abf4c98393d565101a2757cd5
8ijaan <ihsanrfi05@gmail.com>
2026-09-09 14:35:31 +0700

    fix: overlapping ALLOWED_HOSTS

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -34,9 +34,6 @@ SECRET_KEY = 'django-insecure-4ac4pz=7v^#%q^j^0&+d_w71pq2h$^uwm%aiirft(+8%(541p4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
